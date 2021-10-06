c73ebe64da2037ee534aba7464e9b17782b20504
Clint Edwards <cedwards1080@gmail.com>
2021-10-06 15:18:16 -0600

    that should be all of step 2

--- src/app.js
+++ src/app.js
@@ -6,3 +6,12 @@ const app = express();
 
 app.set("views", path.join(__dirname, "./views"));
 app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Index" });
+});
+
+const port = process.env.PORT || "3000";
+app.listen(port, () => {
+  console.log(`PS Project Running on port ${port}!`);
+});

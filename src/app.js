2894161f2f54559f4ed7f4d4a1c5e2f70d254742
Clint Edwards <cedwards1080@gmail.com>
2021-10-06 16:25:09 -0600

    fixing the users json filesync from location

--- src/app.js
+++ src/app.js
@@ -14,14 +14,12 @@ const accountData = fs.readFileSync(
   path.join(__dirname, "json", "accounts.json"),
   "utf8"
 );
-
 const accounts = JSON.parse(accountData);
 
 const userData = fs.readFileSync(
-  path.join(__dirname, "json", "accounts.json"),
+  path.join(__dirname, "json", "users.json"),
   "utf8"
 );
-
 const users = JSON.parse(userData);
 
 app.get("/", (req, res) => {
